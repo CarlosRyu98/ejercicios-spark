dfe42bb26ab9ef676ee19655593ffac416a3db45
CarlosRyu98 <carlos.ivaquero@bosonit.com>
2022-09-09 10:07:19 +0200

    logs
    Signed-off-by: CarlosRyu98 <carlos.ivaquero@bosonit.com>

--- src/main/scala/org/logs/logs.scala
+++ src/main/scala/org/logs/logs.scala
@@ -1,21 +1,8 @@
 package org.logs
 
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
-object logs {
-
-  def main(args: Array[String]): Unit ={
-
-    // Declaración de la sesión de Spark
-    val spark = SparkSession
-      .builder()
-      .master("local")
-      .appName("logs")
-      .getOrCreate()
-
-    // Para ocultar los INFO y WARNING
-    spark.sparkContext.setLogLevel("ERROR")
+object logs extends App with org.sparksession.spark {
 
     // Lectura de los archivos
     val logs_csv = spark.read.csv("src/main/resources/logs/inputs/*")
@@ -40,65 +27,100 @@ object logs {
     val logs_df = logs_df_raw.withColumn("date", to_timestamp(logs_df_raw("date"), "dd/MMM/yyyy:HH:mm:ss"))
     logs_df.show(5)
 
-    //¿Cuáles son los distintos protocolos web utilizados? Agrúpalos.
+  //¿Cuáles son los distintos protocolos web utilizados? Agrúpalos.
+  protocols()
+
+  //¿Cuáles son los códigos de estado más comunes en la web? Agrúpalos y ordénalos para ver cuál es el más común.
+  statusCodes()
+
+  //¿Y los métodos de petición(verbos) más utilizados?
+  requestMethods()
+
+  //¿Qué recurso tuvo la mayor transferencia de bytes de la página web?
+  topSizeResources()
+
+  // Además, queremos saber que recurso de nuestra web es el que más tráfico recibe. Es decir el recurso con más registros en nuestro log
+  topResources()
+
+  //¿Qué días la web recibió más tráfico?
+  topDaysTraffic()
+
+  //¿Cuáles son los hosts son los más frecuentes?
+  hosts()
+
+  //¿A qué horas se produce el mayor número de tráfico en la web?
+  topHourTraffic()
+
+  //¿Cuál es el número de errores 404 que ha habido cada día?
+  errorPerDay()
+
+  private def protocols(): Unit = {
     println("¿Cuáles son los distintos protocolos web utilizados? Agrúpalos.")
     logs_df.select("protocol")
       .distinct()
       .show()
+  }
 
-    //¿Cuáles son los códigos de estado más comunes en la web? Agrúpalos y ordénalos para ver cuál es el más común.
+  private def statusCodes(): Unit = {
     println("¿Cuáles son los códigos de estado más comunes en la web? Agrúpalos y ordénalos para ver cuál es el más común.")
     logs_df.select("http-status-code")
       .groupBy("http-status-code")
       .count()
       .orderBy("http-status-code")
       .show()
+  }
 
-    //¿Y los métodos de petición(verbos) más utilizados?
+  private def requestMethods(): Unit = {
     println("¿Y los métodos de petición(verbos) más utilizados?")
     logs_df.select("request-method")
       .groupBy("request-method")
       .count()
       .orderBy("request-method")
       .show()
+  }
 
-    //¿Qué recurso tuvo la mayor transferencia de bytes de la página web?
+  private def topSizeResources(): Unit = {
     println("¿Qué recurso tuvo la mayor transferencia de bytes de la página web?")
     logs_df.select("resource", "size")
       .orderBy(desc("size"))
       .show(1)
+  }
 
-    // Además, queremos saber que recurso de nuestra web es el que más tráfico recibe. Es decir el recurso con más registros en nuestro log
+  private def topResources(): Unit = {
     println("Además, queremos saber que recurso de nuestra web es el que más tráfico recibe. Es decir el recurso con más registros en nuestro log")
     logs_df.groupBy("resource")
       .count()
       .orderBy(desc("count"))
       .show(1)
+  }
 
-    //¿Qué días la web recibió más tráfico?
+  private def topDaysTraffic(): Unit = {
     println("¿Qué días la web recibió más tráfico?")
     logs_df.select(dayofmonth(logs_df("date")).alias("day"), month(logs_df("date")).alias("month"))
       .groupBy("day", "month")
       .count()
       .orderBy(desc("count"))
       .show()
+  }
 
-    //¿Cuáles son los hosts son los más frecuentes?
+  private def hosts(): Unit = {
     println("¿Cuáles son los hosts son los más frecuentes?")
     logs_df.groupBy("host")
       .count()
       .orderBy(desc("count"))
       .show()
+  }
 
-    //¿A qué horas se produce el mayor número de tráfico en la web?
+  private def topHourTraffic(): Unit = {
     println("¿A qué horas se produce el mayor número de tráfico en la web?")
     logs_df.select(hour(logs_df("date")).alias("hour"))
       .groupBy("hour")
       .count()
       .orderBy(desc("count"))
       .show()
+  }
 
-    //¿Cuál es el número de errores 404 que ha habido cada día?
+  private def errorPerDay(): Unit = {
     println("¿Cuál es el número de errores 404 que ha habido cada día?")
     logs_df.select(col("http-status-code"), dayofmonth(logs_df("date")).alias("day"), month(logs_df("date")).alias("month"))
       .filter(col("http-status-code") === "404")
@@ -106,7 +128,6 @@ object logs {
       .count()
       .orderBy(desc("count"))
       .show()
-
   }
 
 }
